f386a11c755f7aa82baf8da03f96d09e1f308453
Michael Diggin <michael.diggin@gmail.com>
2020-08-19 13:48:19 +0100

    refactored server side and added tests for server

---
+++ backend/server/server_test.go
@@ -0,0 +1,162 @@
+package server
+
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+
+	pb "github.com/michael-diggin/yass/api"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func setUpCacheServer() Server {
+	testCache := make(map[string]string)
+	testCache["testKey"] = "testValue"
+	srv := New(testCache)
+	return srv
+}
+
+func TestAddtoCache(t *testing.T) {
+	logrus.SetOutput(ioutil.Discard) // Discard log output for test
+	srv := setUpCacheServer()
+	tt := []struct {
+		name    string
+		key     string
+		value   string
+		errCode codes.Code
+	}{
+		{"valid case", "newKey", "newValue", codes.OK},
+		{"already set key", "testKey", "testVal", codes.AlreadyExists},
+		{"empty key", "", "emptyVal", codes.InvalidArgument},
+		{"empty value", "key", "", codes.InvalidArgument},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+
+			testKV := &pb.Pair{Key: tc.key, Value: tc.value}
+
+			res, err := srv.Add(context.Background(), testKV)
+			if e, ok := status.FromError(err); ok {
+				if e.Code() != tc.errCode {
+					t.Fatalf("Expected error '%v', got: '%v'", tc.errCode, err)
+				}
+			}
+			if res != nil {
+				if res.Key != testKV.Key {
+					t.Fatalf("Expected %s, got %s", testKV.Key, res.Key)
+				}
+			}
+
+		})
+	}
+}
+
+func TestGetFromCache(t *testing.T) {
+	logrus.SetOutput(ioutil.Discard) // Discard log output for test
+	srv := setUpCacheServer()
+	tt := []struct {
+		name    string
+		key     string
+		value   string
+		errCode codes.Code
+	}{
+		{"valid case", "testKey", "testValue", codes.OK},
+		{"key not found", "newKey", "", codes.NotFound},
+		{"empty key", "", "", codes.InvalidArgument},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+
+			testK := &pb.Key{Key: tc.key}
+
+			res, err := srv.Get(context.Background(), testK)
+			e, ok := status.FromError(err)
+			if !ok {
+				t.Errorf("Could not get code from error: %v", err)
+			}
+			if e.Code() != tc.errCode {
+				t.Fatalf("Expected error '%v', got: '%v'", tc.errCode, err)
+			}
+
+			if res != nil {
+				if res.Value != tc.value {
+					t.Fatalf("Expected %s, got %s", tc.value, res.Key)
+				}
+			}
+
+		})
+	}
+}
+
+func TestResetKeyValue(t *testing.T) {
+	logrus.SetOutput(ioutil.Discard) // Discard log output for test
+	srv := setUpCacheServer()
+	tt := []struct {
+		name    string
+		key     string
+		value   string
+		errCode codes.Code
+	}{
+		{"valid case", "testKey", "newValue", codes.OK},
+		{"key not found", "newKey", "value", codes.NotFound},
+		{"empty key", "", "", codes.InvalidArgument},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+
+			testKV := &pb.Pair{Key: tc.key, Value: tc.value}
+
+			res, err := srv.Reset(context.Background(), testKV)
+			e, ok := status.FromError(err)
+			if !ok {
+				t.Errorf("Could not get code from error: %v", err)
+			}
+			if e.Code() != tc.errCode {
+				t.Fatalf("Expected error '%v', got: '%v'", tc.errCode, err)
+			}
+
+			if res != nil {
+				if res.Key != tc.key {
+					t.Fatalf("Expected %s, got %s", tc.value, res.Key)
+				}
+			}
+
+		})
+	}
+}
+
+func TestDeleteKeyValue(t *testing.T) {
+	logrus.SetOutput(ioutil.Discard) // Discard log output for test
+	srv := setUpCacheServer()
+	tt := []struct {
+		name    string
+		key     string
+		errCode codes.Code
+	}{
+		{"valid case", "testKey", codes.OK},
+		{"key not found", "newKey", codes.NotFound},
+		{"empty key", "", codes.InvalidArgument},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+
+			testKV := &pb.Key{Key: tc.key}
+
+			_, err := srv.Delete(context.Background(), testKV)
+			e, ok := status.FromError(err)
+			if !ok {
+				t.Errorf("Could not get code from error: %v", err)
+			}
+			if e.Code() != tc.errCode {
+				t.Fatalf("Expected error '%v', got: '%v'", tc.errCode, err)
+			}
+
+		})
+	}
+}
