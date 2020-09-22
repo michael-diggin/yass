54854c8fb9f2f4a880d1e8a477d9851b24e075ee
Michael Diggin <michael.diggin@gmail.com>
2020-09-22 21:18:54 +0100

    refactor main.go to use new server

--- backend/cmd/main_test.go
+++ backend/cmd/main_test.go
@@ -2,48 +2,12 @@ package main
 
 import (
 	"context"
-	"net"
 	"testing"
+	"time"
 
 	pb "github.com/michael-diggin/yass/api"
-	"github.com/michael-diggin/yass/backend/pkg/redis"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
 )
-
-func getBufDialer(listener *bufconn.Listener) func(context.Context, string) (net.Conn, error) {
-	return func(ctx context.Context, url string) (net.Conn, error) {
-		return listener.Dial()
-	}
-}
-
-func TestIntegrationServer(t *testing.T) {
-	lis := bufconn.Listen(1024 * 1024)
-	cache, err := redis.New("", "", "localhost:6379") //requires redis running - docker
-	if err != nil {
-		t.Fatalf("Cannot connect to redis for integration test: %v", err)
-	}
-
-	srv := YassServer{}
-	srv.Init(lis, cache)
-	srv.SpinUp()
-	defer srv.ShutDown()
-
-	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx, "", grpc.WithContextDialer(getBufDialer(lis)), grpc.WithInsecure())
-	if err != nil {
-		t.Fatalf("Could not dial server: %err", err)
-	}
-
-	client := pb.NewCacheClient(conn)
-	resp, err := client.Ping(ctx, &pb.Null{})
-	if err != nil {
-		t.Fatalf("Could not send Ping command: %v", err)
-	}
-	if resp.Status != pb.PingResponse_SERVING {
-		t.Fatalf("Server not serving")
-	}
-}
 
 func TestRunServer(t *testing.T) {
 	args := []string{"program", "-p", "8080"}
@@ -55,6 +19,7 @@ func TestRunServer(t *testing.T) {
 		run(args, envFunc)
 	}()
 
+	time.Sleep(50 * time.Millisecond) // Give server time to set up
 	ctx := context.Background()
 	conn, err := grpc.DialContext(ctx, "localhost:8080", grpc.WithInsecure())
 	if err != nil {
