2a1390a73b90746eead67147a7cd86e713587982
Michael Diggin <michael.diggin@gmail.com>
2020-09-23 21:25:08 +0100

    moved client side code to subfolder yass/

---
+++ yass/yass.go
@@ -0,0 +1,65 @@
+// Package yass implements the client side of
+// the gRPC service interface
+package yass
+
+import (
+	"context"
+
+	pb "github.com/michael-diggin/yass/api"
+	"google.golang.org/grpc"
+)
+
+// CacheClient is a struct containing the grpc client
+type CacheClient struct {
+	grpcClient pb.CacheClient
+	conn       *grpc.ClientConn
+}
+
+// NewClient returns a new client that connects to the cache server
+func NewClient(ctx context.Context, addr string) (*CacheClient, error) {
+	conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure()) //TODO: add security and credentials
+	if err != nil {
+		return &CacheClient{}, err
+	}
+	client := pb.NewCacheClient(conn)
+	return &CacheClient{grpcClient: client, conn: conn}, nil
+}
+
+//Close tears down the underlying connection to the server
+func (c CacheClient) Close() error {
+	err := c.conn.Close()
+	return err
+}
+
+// Ping checls if the server and cache are serving
+func (c CacheClient) Ping(ctx context.Context) (bool, error) {
+	resp, err := c.grpcClient.Ping(ctx, &pb.Null{})
+	if resp.Status == pb.PingResponse_SERVING {
+		return true, nil
+	}
+	return false, err
+}
+
+// SetValue sets a key/value pair in the cache
+func (c *CacheClient) SetValue(ctx context.Context, key, value string) error {
+	pair := &pb.Pair{Key: key, Value: value}
+	_, err := c.grpcClient.Set(ctx, pair)
+	return err
+}
+
+// GetValue returns the value of a given key
+func (c *CacheClient) GetValue(ctx context.Context, key string) (string, error) {
+	pbKey := &pb.Key{Key: key}
+	pbPair, err := c.grpcClient.Get(ctx, pbKey)
+	if err != nil {
+		return "", err
+	}
+	return pbPair.Value, nil
+}
+
+// DelValue deletes a key/value pair
+func (c *CacheClient) DelValue(ctx context.Context, key string) error {
+	pbKey := &pb.Key{Key: key}
+	_, err := c.grpcClient.Delete(ctx, pbKey)
+	return err
+}
---
+++ yass/yass_unit_test.go
@@ -0,0 +1,109 @@
+package yass
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/michael-diggin/yass/yass/mocks"
+)
+
+func TestClientPing(t *testing.T) {
+	tt := []struct {
+		name    string
+		serving bool
+	}{
+		{"serving", true},
+		{"not serving", false},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			mockgRPC := &mocks.MockCacheClient{}
+			mockgRPC.PingFn = func() error {
+				if tc.name == "serving" {
+					return nil
+				}
+				return errors.New("Not serving")
+			}
+			cc := CacheClient{grpcClient: mockgRPC, conn: nil}
+			ok, _ := cc.Ping(context.Background())
+			if ok != tc.serving {
+				t.Fatalf("Serving status wrong")
+			}
+			if !mockgRPC.PingInvoked {
+				t.Fatalf("Ping function never called")
+			}
+		})
+	}
+}
+
+func TestClientSetValue(t *testing.T) {
+	mockgRPC := &mocks.MockCacheClient{}
+	mockgRPC.SetFn = func(ctx context.Context, key, value string) error {
+		return nil
+	}
+	cc := CacheClient{grpcClient: mockgRPC, conn: nil}
+	key := "test"
+	val := "value"
+	err := cc.SetValue(context.TODO(), key, val)
+	if err != nil {
+		t.Fatalf("Non nil err: %v", err)
+	}
+	if !mockgRPC.SetInvoked {
+		t.Fatalf("Add method was not invoked")
+	}
+}
+
+var errTest = errors.New("Not in Cache")
+
+func TestClientGetValue(t *testing.T) {
+	tt := []struct {
+		name  string
+		key   string
+		value string
+		err   error
+	}{
+		{"valid case", "test", "value", nil},
+		{"err case", "bad", "", errTest},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			mockgRPC := &mocks.MockCacheClient{}
+			mockgRPC.GetFn = func(ctx context.Context, key string) (string, error) {
+				if key == "test" {
+					return "value", nil
+				}
+				return "", errTest
+			}
+			cc := CacheClient{grpcClient: mockgRPC, conn: nil}
+			val, err := cc.GetValue(context.TODO(), tc.key)
+			if err != tc.err {
+				t.Fatalf("Unexpected err: %v", err)
+			}
+			if val != tc.value {
+				t.Fatalf("Expected '%s', got %s", tc.value, val)
+			}
+			if !mockgRPC.GetInvoked {
+				t.Fatalf("Get method was not invoked")
+			}
+		})
+	}
+}
+
+func TestClientDelValue(t *testing.T) {
+	mockgRPC := &mocks.MockCacheClient{}
+	mockgRPC.DelFn = func(ctx context.Context, key string) error {
+		return nil
+	}
+	cc := CacheClient{grpcClient: mockgRPC, conn: nil}
+	key := "test"
+	err := cc.DelValue(context.TODO(), key)
+	if err != nil {
+		t.Fatalf("Non nil err: %v", err)
+	}
+	if !mockgRPC.DelInvoked {
+		t.Fatalf("Del method was not invoked")
+	}
+}
