25a3c25b96cf0c80f6983efb141c1460796c33c6
michael-diggin <50772101+michael-diggin@users.noreply.github.com>
2020-12-27 11:26:34 +0000

    graceful shutdown of gatewat server (#26)

--- gateway/api/app.go
+++ gateway/api/app.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"context"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -10,49 +12,52 @@ import (
 
 // Gateway holds the router and the grpc clients
 type Gateway struct {
-	Router     *mux.Router
+	*http.Server
 	Clients    map[int]GrpcClient
 	numServers int
 	mu         sync.RWMutex
 }
 
 // NewGateway will initialize the application
-func NewGateway(numServers int) *Gateway {
+func NewGateway(numServers int, srv *http.Server) *Gateway {
 	g := Gateway{}
-	g.Router = mux.NewRouter()
-	g.initializeAPIRoutes()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/get/{key}", g.Get).Methods("GET")
+	router.HandleFunc("/set", g.Set).Methods("POST")
+	router.HandleFunc("/delete/{key}", g.Delete).Methods("DELETE")
+	router.HandleFunc("/register", g.RegisterCacheServer).Methods("POST")
+
+	g.Server = srv
+
+	g.Handler = router
+
 	g.Clients = make(map[int]GrpcClient)
 	g.numServers = numServers
 	g.mu = sync.RWMutex{}
 	return &g
 }
 
-func (g *Gateway) initializeAPIRoutes() {
-	g.Router.HandleFunc("/get/{key}", g.Get).Methods("GET")
-	g.Router.HandleFunc("/set", g.Set).Methods("POST")
-	g.Router.HandleFunc("/delete/{key}", g.Delete).Methods("DELETE")
-	g.Router.HandleFunc("/register", g.RegisterCacheServer).Methods("POST")
-}
-
-//Serve will start the application
-func (g *Gateway) Serve(addr string) <-chan error {
-	errChan := make(chan error, 1)
-	errChan <- http.ListenAndServe(addr, g)
-	return errChan
-}
-
 //ServeHTTP will serve and route a request
 func (g *Gateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	g.Router.ServeHTTP(w, r)
+	g.Handler.ServeHTTP(w, r)
 }
 
-// ShutDown will close all grpc connections the gateway holds
-func (g *Gateway) ShutDown() {
-	logrus.Infof("Closing connections to storage servers")
+// Stop will close all grpc connections the gateway holds
+func (g *Gateway) Stop() {
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := g.Shutdown(ctxShutDown); err != nil {
+		logrus.Fatalf("server shutdown failed:%v", err)
+	}
+	logrus.Info("gateway server stopped")
+
 	g.mu.Lock()
 	for num, client := range g.Clients {
 		serverNum := num
 		client.Close()
 		delete(g.Clients, serverNum)
 	}
+	logrus.Info("Closed connections to storage servers")
 }
--- gateway/main.go
+++ gateway/main.go
@@ -1,54 +1,53 @@
 package main
 
 import (
-	"context"
 	"flag"
+	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/michael-diggin/yass/gateway/api"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	var port *string
+	var port *int
 	var numServers *int
-	port = flag.String("p", ":8010", "port for server to listen on")
+	port = flag.Int("p", 8010, "port for server to listen on")
 	numServers = flag.Int("s", 3, "the initial number of storage servers")
 	flag.Parse()
 
-	gateway := api.NewGateway(*numServers)
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", *port),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	gateway := api.NewGateway(*numServers, srv)
 
-	defer gateway.ShutDown()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+	defer gateway.Stop()
 
 	go func() {
-		logrus.Infof("Running the server on port %s", *port)
-		select {
-		case <-ctx.Done():
-		case err := <-gateway.Serve(*port):
+		logrus.Infof("Running the server on port %d", *port)
+		err := gateway.ListenAndServe()
+		if err != nil {
 			logrus.Errorf("encountered error when running gateway: %v", err)
 		}
-		wg.Done()
 	}()
 
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
 		// Check for a closing signal
 		sigChan := make(chan os.Signal, 1)
 		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-		select {
-		case <-ctx.Done():
-		case sig := <-sigChan:
-			logrus.Infof("OS signal caught: %+v", sig)
-			cancel()
-			return
-		}
+		sig := <-sigChan
+		logrus.Infof("OS signal caught: %+v", sig)
+		wg.Done()
 	}()
 
-	wg.Wait()
+	wg.Wait() // wait for sever go routine to exit
 }
