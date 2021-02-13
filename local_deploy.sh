88b3472d942ce6cc966923881fdad50a35455400
michael-diggin <50772101+michael-diggin@users.noreply.github.com>
2021-02-13 09:18:00 +0000

    Implement yasscli (#60)
    * clean up local deploy script
    
    * logic for cli parsing and command processing
    
    * tests for interpreter

--- local_deploy.sh
+++ local_deploy.sh
@@ -1,22 +1,47 @@
 #! /bin/bash
 # Simple utility script to spin up the watchtower and 3 server nodes on docker
 
-# build the docker images
-docker build -t yass-server:0.2 -f server/Dockerfile .
-docker build -t yass-watchtower:0.1 -f watchtower/Dockerfile .
+local_build=false
 
-# firstly create the docker network
-docker network create yass-net
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -b|--build) local_build=true; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ "$(docker images -q local-server 2> /dev/null)" == "" ]] || [[ "$(docker images -q local-watchtower 2> /dev/null)" == "" ]]; then
+  echo "Docker images not found locally"
+  local_build=true
+fi
+
+# build the docker images
+if [ "$local_build" = true ]; then
+    echo "Building docker images..."
+    docker build -t local-server -f server/Dockerfile .
+    docker build -t local-watchtower -f watchtower/Dockerfile .
+fi
+
+# firstly create the docker network if it doesn't exist
+if [[ "$(docker network inspect yass-net >/dev/null 2>&1)" ]]; then
+    echo "Creating docker network \`yass-net\`..."
+    docker network create yass-net
+fi
 
 # watchtower needs to be deployed first
-docker run -d --name watchtower -p 8010:8010 --network yass-net yass-watchtower:0.1 -f "node_data.txt"
+echo "deploying watchtower..."
+docker run -d --name watchtower -p 8010:8010 --network yass-net local-watchtower -f "node_data.txt" >/dev/null
+
 
 # next the 3 server nodes
-docker run -d --name server-0 --env POD_NAME=server-0 -p 8080:8080 --network yass-net yass-server:0.2 -g "watchtower:8010" -p 8080
-docker run -d --name server-1 --env POD_NAME=server-1 -p 8081:8081 --network yass-net yass-server:0.2 -g "watchtower:8010" -p 8081
-docker run -d --name server-2 --env POD_NAME=server-2 -p 8082:8082 --network yass-net yass-server:0.2 -g "watchtower:8010" -p 8082
+for i in {0..2};
+do
+echo "deploying server-$i..." 
+docker run -d --name server-$i --env POD_NAME=server-$i -p 808$i:808$i --network yass-net local-server -g "watchtower:8010" -p 808$i >/dev/null;
+done
 
-echo -e "\nAccessible on:"
+echo -e "\nYass Servers accessible on:"
 for i in {0..2};
 do echo "$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server-$i):808$i";
 done
---
+++ yasscli/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/michael-diggin/yass"
+	"github.com/michael-diggin/yass/yasscli/interpreter"
+)
+
+func main() {
+	nodeAddress := flag.String("l", "localhost:8080", "location of a yass server node")
+	flag.Parse()
+
+	ctx := context.Background()
+	client, err := yass.NewClient(ctx, *nodeAddress)
+	if err != nil {
+		fmt.Printf("Could not connect to yass server: %v", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+	interpreter := interpreter.New(client)
+
+	fmt.Println("Welcome to the YassCLI interpreter")
+	fmt.Println("Enter \\halp for help, \\q to quit")
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("\nyasscli=> ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Encountered error when reading command: %v", err)
+		}
+		command := strings.TrimSpace(text)
+		if command == "" {
+			continue
+		}
+
+		exit := interpreter.ProcessCommand(command, os.Stdout)
+		if exit {
+			break
+		}
+	}
+}
