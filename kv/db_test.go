6565ae6cad76fb4fc9de252dbd166c8360b7d194
michael-diggin <50772101+michael-diggin@users.noreply.github.com>
2021-08-05 19:36:05 +0100

    reset data store from persistent log on start up (#73)

--- kv/db_test.go
+++ kv/db_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/michael-diggin/yass/api"
+	"github.com/michael-diggin/yass/log"
 	"github.com/stretchr/testify/require"
 )
 
@@ -50,4 +51,39 @@ func TestKVDBNotFound(t *testing.T) {
 	require.Error(t, err)
 	require.Nil(t, read)
 	require.True(t, errors.As(err, &api.ErrNotFound{}))
+}
+
+func TestResetOnStartUp(t *testing.T) {
+	dir, err := ioutil.TempDir("", "store-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	c := log.Config{}
+	c.Segment.MaxStoreBytes = 32
+	plog, err := log.NewLog(dir, c)
+	require.NoError(t, err)
+
+	// append two records
+	appendOne := &api.Record{
+		Id:    "key-1",
+		Value: []byte("hello world again"),
+	}
+	off, err := plog.Append(appendOne)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	appendTwo := &api.Record{
+		Id:    "key-2",
+		Value: []byte("hello world again"),
+	}
+	off, err = plog.Append(appendTwo)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1), off)
+
+	data, err := resetOnStartUp(plog)
+	require.NoError(t, err)
+
+	require.Len(t, data, 2)
+	require.Equal(t, appendOne.Value, data[appendOne.Id].Value)
+	require.Equal(t, appendTwo.Value, data[appendTwo.Id].Value)
+
 }
